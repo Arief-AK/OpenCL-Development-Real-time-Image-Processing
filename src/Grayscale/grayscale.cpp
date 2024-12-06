50843304741f20c511fcc22fa79d8f0bf2a1ee67
arief-ak <mariefak22@gmail.com>
2024-12-06 12:19:55 +0100

    added OpenCL and OpenCV configuration to Grayscale

--- src/Grayscale/grayscale.cpp
+++ src/Grayscale/grayscale.cpp
@@ -1,7 +1,14 @@
 #include <CL/cl.h>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 int main(int, char**){
     std::cout << "Hello, from OpenCL-Dev-Realtime-Image-Processing!\n";
+    
+    std::string imagePath = "galaxy.jpg";
+    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+    cv::imshow("Display Window", image);
+    cv::waitKey(0);
+    
     return 0;
 }
